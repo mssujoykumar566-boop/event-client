b88b61b7691eed29abe0a2e2d333ce3e1dd649f5
mssujoykumar566-boop <mssujoykumar566@gmail.com>
2026-07-16 04:45:21 +0600

    login ussue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const backendApiUrl =
+  process.env.BACKEND_API_URL ?? "https://event-server-nvtu.onrender.com/api";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${backendApiUrl}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,9 +3,6 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL:
-    "https://event-server-nvtu.onrender.com/api/auth",
-
   fetchOptions: {
     credentials: "include",
   },
